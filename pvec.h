686cc79e18e7ec7725be67cdd24d6b3ab11f1d29
jekky <11986158+jac3km4@users.noreply.github.com>
2024-01-29 02:39:57 +0000

    Allocation and sharing optimizations

--- pvec.h
+++ pvec.h
@@ -1,13 +1,36 @@
 #include <kklib.h>
 
-static inline kk_vector_t kk_vector_updated(kk_vector_t vec, int32_t index,
+static inline kk_vector_t kk_vector_updated(kk_vector_t vec, size_t index,
                                             kk_box_t elem, kk_context_t *ctx) {
 
-  kk_assert(index >= 0 && index < kk_vector_len(vec, ctx));
+  size_t old_len = kk_vector_len_borrow(vec, ctx);
+  assert(index >= 0 && index <= old_len);
 
-  kk_vector_t copy = kk_vector_copy(vec, ctx);
-  kk_box_t *buf = kk_vector_buf_borrow(copy, NULL, ctx);
-  kk_box_drop(buf[index], ctx);
-  buf[index] = elem;
+  if (index < old_len && kk_datatype_is_unique(vec, ctx)) {
+    // we can modify the trie in place when it's not shared
+    kk_box_t *buf = kk_vector_buf_borrow(vec, NULL, ctx);
+    kk_box_drop(buf[index], ctx);
+    buf[index] = elem;
+    return vec;
+  }
+  size_t new_len = old_len;
+  if (index == old_len) {
+    new_len *= 2;
+  }
+  kk_vector_t copy = kk_vector_alloc_uninit(new_len, NULL, ctx);
+  kk_box_t *old_buf = kk_vector_buf_borrow(vec, NULL, ctx);
+  kk_box_t *new_buf = kk_vector_buf_borrow(copy, NULL, ctx);
+
+  for (size_t i = 0; i < new_len; i++) {
+    if (i == index) {
+      new_buf[i] = elem;
+    } else if (i < old_len) {
+      new_buf[i] = kk_box_dup(old_buf[i], ctx);
+    } else {
+      new_buf[i] = kk_box_null();
+    }
+  }
+
+  kk_vector_drop(vec, ctx);
   return copy;
 }
